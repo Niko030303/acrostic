a48769bb381b84a1b87a13de5ed2abbcb11807e8
yangweiming <y1257891258@qq.com>
2023-07-15 14:49:26 +0800

    feat: add vercel web analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,10 +2,12 @@ import '../styles/globals.css'
 import 'remixicon/fonts/remixicon.css'
 import type { AppProps } from 'next/app'
 import Script from 'next/script'
+import { Analytics } from '@vercel/analytics/react';
 
 export default function App({ Component, pageProps }: AppProps) {
   return <>
   <Component {...pageProps} />
+  <Analytics />
   <Script src="https://cdnjs.buymeacoffee.com/1.0.0/button.prod.min.js" data-name="bmc-button" data-slug="niko030303" data-color="#FFDD00" data-emoji="" data-font="Lato" data-text="Buy me a coffee" data-outline-color="#000000" data-font-color="#000000" data-coffee-color="#ffffff" />
   </>
 }
